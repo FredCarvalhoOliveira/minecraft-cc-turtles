617de8c273906807e2521d119b99e48ec4fe6e1c
dfAndrade <36664957+dfAndrade@users.noreply.github.com>
2024-02-24 20:39:49 +0000

    Update install.lua

--- lua/install.lua
+++ lua/install.lua
@@ -18,17 +18,24 @@ create_dir("progs")
  
 -- Our program libraries
 create_dir("libs")
- 
--- move json lib
-forceCopy("/disk/json.lua", "libs/json.lua")
+
+create_dir("libs/lua")
+create_dir("libs/python")
  
 -- Move git into progs  
 forceCopy("/disk/git", "/progs/git")
  
 -- Add the folder to shell path
--- shell.setPath(shell.path()..":/myProgs")
+shell.setPath(shell.path()..":/progs")
  
--- Retrieve git "pull" capabilities
-shell.run("git", "get", "dfAndrade", "cc-repo", "main", "git_utils.lua", "/myProgs/git_utils.lua")
- 
--- TODO Pull init packages by pulling specific folder
+-- Get JSON capabilities
+shell.run("git", "get", "dfAndrade", "cc-repo", "main", "utils/json.lua", "/libs/lua/json.lua")
+
+-- Get git "pull" capabilities
+shell.run("git", "get", "dfAndrade", "cc-repo", "main", "git_utils.lua", "/prog/git_utils")
+
+-- Get startup file
+shell.run("git", "get", "FredCarvalhoOliveira", "minecraft-cc-turtles", "master", "lua/startup.lua", "startup")
+
+
+-- TODO download python programs and alias them a
