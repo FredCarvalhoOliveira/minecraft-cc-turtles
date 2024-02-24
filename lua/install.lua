48f0537f6cdb4b729b65001e799dd59b1cbcd804
dfAndrade <36664957+dfAndrade@users.noreply.github.com>
2024-02-24 22:36:25 +0000

    Update install.lua

--- lua/install.lua
+++ lua/install.lua
@@ -3,7 +3,15 @@ function create_dir(path)
         fs.makeDir(path)
     end
 end
- 
+
+function create_file_rel(path)
+    fs.open(shell.resolve(path), "a").close()
+end
+
+function create_file(path)
+    fs.open(path, "a").close()
+end
+
 function forceCopy(source, target)
     if fs.exists(target) then
         fs.delete(target)
@@ -15,10 +23,13 @@ end
 -- Create your own "rom" folder
 -- that will be added to PATH
 create_dir("progs")
+
+--Create .git
+create_dir(".git")
+create_file("/.git/state")
  
 -- Our program libraries
 create_dir("libs")
-
 create_dir("libs/lua")
 create_dir("libs/python")
  
@@ -37,5 +48,7 @@ shell.run("git", "get", "dfAndrade", "cc-repo", "main", "git_utils.lua", "/progs
 -- Get startup file
 shell.run("git", "get", "FredCarvalhoOliveira", "minecraft-cc-turtles", "master", "lua/startup.lua", "startup")
 
-
--- TODO download python programs and alias them a
+-- Get python from server
+-- Success: "> Downloaded as progs/py"
+-- Failure: "> Connecting to ... Failed"
+shell.run("wget", "http://127.0.0.1:8080/", "progs/py")
