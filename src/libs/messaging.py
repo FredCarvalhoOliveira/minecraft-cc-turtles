47652580ca56b282b8d65f534ba8e78e5a34d02c
dfAndrade <diogo.freire.dandrade@gmail.com>
2024-02-29 21:46:28 +0000

    The future

--- src/libs/messaging.py
+++ src/libs/messaging.py
@@ -3,7 +3,7 @@ from cc import peripheral
 class MessagingController:
     def __init__(self, id) -> None:
         self.id = id
-        self.modem = peripheral.find("modem")
+        self.modem = peripheral.wrap("right")
         print(self.modem)
 
 
